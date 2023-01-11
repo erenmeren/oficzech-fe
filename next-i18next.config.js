46be3127c72af76e2fe7d0d87fd5d4850eb42325
kunteper <kunteper@kt.com>
2023-01-11 15:08:22 +0000

    Slayt

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -8,13 +8,11 @@ module.exports = {
   fallbackLng: {
     default: ["tr"],
   },
-  // localPath: path.resolve("./public/locales"),
   react: { useSuspense: false },
   localePath:
     typeof window === "undefined"
       ? path.resolve("./public/locales")
       : "/locales",
-
   debug: false,
   reloadOnPrerender: process.env.NODE_ENV === "development",
 };
